a2b90d111d4a3fd827e39bb05ab3a6ec496726a7
Clemente Paredes <clparede@dcc.uchile.cl>
2018-11-24 02:05:28 -0300

    To Do: Finish readme Complete test

--- src/test/java/Tarea2Test.java
+++ src/test/java/Tarea2Test.java
@@ -1,13 +1,13 @@
 import controller.Game;
 import facade.HomeworkTwoFacade;
-import logic.brick.Brick;
-import logic.brick.GlassBrick;
-import logic.brick.MetalBrick;
-import logic.brick.WoodenBrick;
+import logic.brick.*;
 import logic.level.Level;
+import logic.level.NullLevel;
 import logic.level.PlayableLevel;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.ArrayList;
 
 import static org.junit.Assert.*;
 
@@ -20,6 +20,8 @@ public class Tarea2Test {
     private Brick glassBrick;
     private Brick woodenBrick;
     private Brick metalBrick;
+    private Brick nullBrick;
+    private Level level0;
     private Level level1;
     private Level level2;
     private Level level3;
@@ -32,6 +34,8 @@ public class Tarea2Test {
         glassBrick = new GlassBrick();
         woodenBrick = new WoodenBrick();
         metalBrick = new MetalBrick();
+        nullBrick = new NullBrick();
+        level0 = new NullLevel();
         level1 = new PlayableLevel("Level 1", 10, 0.5, 0.5, seed);
         level2 = new PlayableLevel("Level 2", 10, 0.5, 0.0, seed);
         level3 = new PlayableLevel("Level 3", 10, 1, 0.5, seed);
@@ -44,6 +48,7 @@ public class Tarea2Test {
         assertFalse(glassBrick.isDestroyed());
         assertFalse(woodenBrick.isDestroyed());
         assertFalse(metalBrick.isDestroyed());
+        assertTrue(nullBrick.isDestroyed());
         glassBrick.hit();
         assertTrue(glassBrick.isDestroyed());
         int i = 0;
@@ -72,10 +77,21 @@ public class Tarea2Test {
     @Test
     public void testLevel(){
         assertEquals("Level 1", level1.getName());
+        assertEquals(0, level0.getNumberOfBricks());
         assertEquals(16, level1.getNumberOfBricks());
         assertEquals(10, level2.getNumberOfBricks());
         assertEquals(16, level3.getNumberOfBricks());
         assertEquals(10, level4.getNumberOfBricks());
+        assertEquals(new ArrayList<>(), level0.getBricks());
+        assertFalse(level0.isPlayableLevel());
+        assertTrue(level1.isPlayableLevel());
+        assertFalse(level0.hasNextLevel());
+        assertFalse(level1.hasNextLevel());
+        level0.addPlayingLevel(level1).addPlayingLevel(level2);
+        assertFalse(level0.hasNextLevel());
+        assertTrue(level1.hasNextLevel());
+        assertEquals("Level 2", level1.getNextLevel().getName());
+        assertEquals("", level2.getNextLevel().getName());
     }
 
     @Test
